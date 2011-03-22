eac689aa631734802c0dfbbf4f654c67cad7071f
William Rufino <williamhrs@gmail.com>
2011-03-22 09:28:25 -0300

    Config file created

---
+++ config/assets.php
@@ -0,0 +1,10 @@
+<?php 
+/*
+* Path to the assets folder, it must be relative to the index.php file
+* @author William Rufino
+* @since 22/03/2010
+*
+*/
+$config['css_path'] = 'public/css/';
+$config['js_path'] = 'public/js/';
+$config['image_path'] = 'public/images/';
